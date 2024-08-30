3e2a31204a0430e2099676621faa4cbe92913560
Investiment Security <richtradereps@gmail.com.com>
2024-08-30 17:07:42 +0100

    Atualizar o servidor para suportar várias criptomoedas

--- Pasta JS/index.js
+++ Pasta JS/index.js
@@ -1,4 +1,14 @@
-document.getElementById('menu-icon').addEventListener('click', function () {
-    const navList = document.getElementById('nav-list');
-    navList.classList.toggle('show');
+// Função para controlar a exibição do menu em dispositivos móveis
+const menuIcon = document.querySelector('.menu-icon');
+const navList = document.querySelector('.nav-list');
+
+menuIcon.addEventListener('click', () => {
+    navList.classList.toggle('show'); // Alterna a exibição do menu
+});
+
+// Função para ocultar o menu em telas maiores
+window.addEventListener('resize', () => {
+    if (window.innerWidth > 768) {
+        navList.classList.remove('show'); // Oculta o menu se a tela for maior que 768px
+    }
 });
