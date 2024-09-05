b96d4dd5feb9f837df976411f7448774d813185b
Investiment Security <richtradereps@gmail.com.com>
2024-09-05 15:19:02 +0100

    Corrigir menu do celular e adicionar melhorias

--- Pasta JS/index.js
+++ Pasta JS/index.js
@@ -7,16 +7,6 @@ menuIcon.addEventListener('click', function () {
     navList.classList.toggle('show'); // Exibe ou oculta o menu
 });
 
-// Fecha o menu se clicar fora dele
-document.addEventListener('click', function (event) {
-    const target = event.target; // O elemento que foi clicado
-
-    // Verifica se o clique foi fora do menu ou do ícone do menu
-    if (!navList.contains(target) && !menuIcon.contains(target)) {
-        navList.classList.remove('show'); // Fecha o menu
-    }
-});
-
 // Oculta o menu ao redimensionar a janela
 window.addEventListener('resize', function () {
     if (window.innerWidth > 768) {
