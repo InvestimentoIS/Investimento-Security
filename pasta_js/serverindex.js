524c202d7a6d7404bc34488205b8bc46baaeff4e
Investiment Security <richtradereps@gmail.com.com>
2024-09-11 16:07:27 +0100

    Corrigir scripts e adicionar melhorias

--- pasta_js/serverindex.js
+++ pasta_js/serverindex.js
@@ -7,15 +7,19 @@ const bcrypt = require('bcryptjs');
 const nodemailer = require('nodemailer');
 const path = require('path');
 const session = require('express-session'); // Suporte para sessões
-const multer = require('multer'); // Importando multer para upload de arquivos
-const fs = require('fs'); // Importando fs para manipulação de arquivos e diretórios
-const MongoStore = require('connect-mongo'); // Usando MongoDB como store de sessões
+const multer = require('multer'); // Upload de arquivos
+const fs = require('fs'); // Manipulação de arquivos
+const MongoStore = require('connect-mongo'); // Store de sessões no MongoDB
 
 // Configurando o aplicativo Express
 const app = express();
 
 // Habilitar CORS
-app.use(cors());
+const corsOptions = {
+  origin: 'https://investimentois.github.io/Investimento-Security/',  // Domínio do seu front-end
+  optionsSuccessStatus: 200
+};
+app.use(cors(corsOptions));
 
 // Middleware para analisar o corpo da requisição
 app.use(bodyParser.json());
@@ -24,7 +28,7 @@ app.use(bodyParser.json());
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    tlsInsecure: true // Desabilita a verificação SSL (apenas para teste, não recomendado em produção)
+    tlsInsecure: true // Desabilita a verificação SSL (somente para testes)
 }).then(() => {
     console.log('Conectado ao MongoDB');
 }).catch((err) => {
@@ -37,10 +41,10 @@ app.use(session({
     resave: false,
     saveUninitialized: false,
     store: MongoStore.create({
-        mongoUrl: process.env.MONGO_URI, // Usa o MongoDB como store de sessões
-        collectionName: 'sessions', // Nome da coleção onde as sessões serão armazenadas
+        mongoUrl: process.env.MONGO_URI, // MongoDB para sessões
+        collectionName: 'sessions', // Coleção onde as sessões são armazenadas
     }),
-    cookie: { secure: false } // Defina como true se estiver usando HTTPS em produção
+    cookie: { secure: false } // Defina como true se usar HTTPS em produção
 }));
 
 // Definindo o caminho relativo para a pasta 'uploads'
@@ -48,7 +52,7 @@ const uploadDir = path.join(__dirname, 'uploads');
 
 // Verifica se a pasta 'uploads/' existe, e a cria se não existir
 if (!fs.existsSync(uploadDir)) {
-    fs.mkdirSync(uploadDir, { recursive: true }); // Inclui 'recursive: true' para criar diretórios pai, se necessário
+    fs.mkdirSync(uploadDir, { recursive: true });
     console.log(`Diretório 'uploads' criado em ${uploadDir}`);
 }
 
@@ -58,10 +62,10 @@ const UserSchema = new mongoose.Schema({
     email: { type: String, unique: true, required: true },
     password: { type: String, required: true },
     isVerified: { type: Boolean, default: false },
-    profilePhoto: { type: String, default: '/uploads/default-profile.png' },  // Foto de perfil
-    country: { type: String, default: 'Brasil' },                             // País do usuário
-    birthdate: { type: String, default: '01/01/2000' },                       // Data de nascimento
-    phone: { type: String, default: '(11) 99999-9999' }                       // Telefone do usuário
+    profilePhoto: { type: String, default: '/uploads/default-profile.png' },
+    country: { type: String, default: 'Brasil' },
+    birthdate: { type: String, default: '01/01/2000' },
+    phone: { type: String, default: '(11) 99999-9999' }
 });
 
 const User = mongoose.model('User', UserSchema);
@@ -78,7 +82,6 @@ app.get('/meu-perfil', async (req, res) => {
             return res.status(404).json({ error: 'Usuário não encontrado' });
         }
 
-        // Enviar os dados do usuário para o front-end
         res.status(200).json({
             username: user.username,
             email: user.email,
@@ -95,7 +98,7 @@ app.get('/meu-perfil', async (req, res) => {
 // Configuração do Multer para upload de imagem de perfil
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, uploadDir); // Pasta onde as imagens serão salvas
+        cb(null, uploadDir); // Pasta para uploads
     },
     filename: (req, file, cb) => {
         cb(null, `${req.session.userId}-${Date.now()}${path.extname(file.originalname)}`);
@@ -126,28 +129,23 @@ app.post('/register', async (req, res) => {
     try {
         const { username, email, password, confirm_password, country, birthdate, phone } = req.body;
 
-        // Verificar se as senhas coincidem
         if (password !== confirm_password) {
             return res.status(400).json({ error: "As senhas não coincidem." });
         }
 
-        // Verificar se o nome de usuário já existe
         const userExists = await User.findOne({ username });
         if (userExists) {
             return res.status(400).json({ error: "Nome de usuário já existe." });
         }
 
-        // Verificar se o e-mail já existe
         const emailExists = await User.findOne({ email });
         if (emailExists) {
             return res.status(400).json({ error: "E-mail já cadastrado." });
         }
 
-        // Criptografar a senha
         const salt = await bcrypt.genSalt(10);
         const hashedPassword = await bcrypt.hash(password, salt);
 
-        // Criar o novo usuário
         const newUser = new User({
             username,
             email,
@@ -159,7 +157,6 @@ app.post('/register', async (req, res) => {
 
         await newUser.save();
 
-        // Enviar o e-mail de verificação
         sendVerificationEmail(newUser);
 
         res.status(201).json({ success: "Cadastro bem-sucedido! Verifique seu e-mail." });
@@ -180,7 +177,8 @@ function sendVerificationEmail(user) {
         debug: true
     });
 
-    const verificationUrl = `http://localhost:3003/verify?userId=${user._id}`;
+    const verificationUrl = `https://investimento-security.onrender.com/verify?userId=${user._id}`;
+
     const mailOptions = {
         from: process.env.EMAIL_USER,
         to: user.email,
@@ -188,27 +186,13 @@ function sendVerificationEmail(user) {
         html: `
             <div style="font-family: Arial, sans-serif; text-align: center; padding: 20px; color: #333;">
                 <h1 style="color: #4CAF50;">Seja bem-vindo!</h1>
-                
-                <img src="cid:logo-image" alt="Logo" style="width: 100px; margin-bottom: 20px;" />
-        
-                <p style="font-size: 18px;">Estamos muito felizes por você estar aqui.</p>
-                <p style="font-size: 16px;">Para completar seu cadastro e ativar sua conta, clique no botão abaixo:</p>
+                <p>Para completar seu cadastro e ativar sua conta, clique no botão abaixo:</p>
                 <a href="${verificationUrl}" 
-                   style="display: inline-block; background-color: #4CAF50; color: white; padding: 10px 20px; text-decoration: none; border-radius: 5px; font-size: 16px;">
+                   style="display: inline-block; background-color: #4CAF50; color: white; padding: 10px 20px; text-decoration: none; border-radius: 5px;">
                    Verificar E-mail
                 </a>
-                <p style="margin-top: 20px; font-size: 14px;">Se você não se cadastrou em nossa plataforma, ignore este e-mail.</p>
-                <p style="color: #888; font-size: 12px; margin-top: 20px;">&copy; 2024 Sua Empresa. Todos os direitos reservados.</p>
             </div>
         `,
-        attachments: [
-            {
-                filename: 'logo_email.jpg',
-                path: path.join(__dirname, 'fotos', 'vecteezy_trader-holding-graph-chart-with-arrow-for-analysis-stock_7039565.jpg'),
-                cid: 'logo-image',
-                contentType: 'image/jpeg'
-            }
-        ]
     };
 
     transporter.sendMail(mailOptions, (err, info) => {
@@ -230,11 +214,9 @@ app.get('/verify', async (req, res) => {
             return res.status(400).json({ error: "Usuário não encontrado." });
         }
 
-        // Verificar o e-mail do usuário
         user.isVerified = true;
         await user.save();
 
-        // Redireciona para a página de login após verificar o e-mail
         res.redirect('/login.html');
     } catch (error) {
         res.status(500).json({ error: "Erro ao verificar e-mail." });
@@ -246,24 +228,20 @@ app.post('/login', async (req, res) => {
     const { email, password } = req.body;
 
     try {
-        // Verifica se o usuário existe
         const user = await User.findOne({ email });
         if (!user) {
             return res.status(400).json({ error: 'Usuário não encontrado' });
         }
 
-        // Verifica se a senha está correta
         const isMatch = await bcrypt.compare(password, user.password);
         if (!isMatch) {
             return res.status(400).json({ error: 'Senha incorreta' });
         }
 
-        // Verifica se o e-mail foi verificado
         if (!user.isVerified) {
             return res.status(400).json({ error: 'E-mail não verificado. Verifique seu e-mail antes de fazer login.' });
         }
 
-        // Salva os dados do usuário na sessão
         req.session.userId = user._id;
         req.session.username = user.username;
 
@@ -296,14 +274,14 @@ app.post('/logout', (req, res) => {
         if (err) {
             return res.status(500).json({ error: 'Erro ao fazer logout. Tente novamente.' });
         }
-        res.clearCookie('connect.sid'); // Limpa o cookie de sessão
+        res.clearCookie('connect.sid');
         res.status(200).json({ message: 'Logout bem-sucedido!' });
     });
 });
 
-// Verificação se o arquivo index.html existe para servir como página inicial
+// Servir a página inicial
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../index.html')); // Corrigido para apontar um diretório acima
+    res.sendFile(path.join(__dirname, '../index.html'));
 });
 
 // Inicialização do servidor
