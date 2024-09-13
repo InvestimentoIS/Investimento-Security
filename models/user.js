4f93686dd3e745940263689970ec2407803979fe
Investiment Security <richtradereps@gmail.com.com>
2024-09-13 19:15:16 +0100

    Corrigir scripts e adicionar melhorias

---
+++ models/user.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    username: { type: String, unique: true, required: true },
+    email: { type: String, unique: true, required: true },
+    password: { type: String, required: true },
+    isVerified: { type: Boolean, default: false },
+    profilePhoto: { type: String, default: '/uploads/default-profile.png' },
+    country: { type: String, default: 'Brasil' },
+    birthdate: { type: String, default: '01/01/2000' },
+    phone: { type: String, default: '(11) 99999-9999' }
+});
+
+module.exports = mongoose.model('User', UserSchema);
